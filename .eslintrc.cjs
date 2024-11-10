4fcf12f77b0dce278bf3fe7f5d01c35144b0ae7f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-10 13:49:36 +0100

    Initial commit from create-remix

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'simple-import-sort',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-duplicate-imports': 'error',
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 100,
+};
