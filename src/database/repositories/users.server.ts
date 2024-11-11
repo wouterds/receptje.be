8756c1be7f082631e0a405e9e4e3d58c81e2e000
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-11 17:44:54 +0100

    feat: encode ids

--- src/database/repositories/users.server.ts
+++ src/database/repositories/users.server.ts
@@ -1,6 +1,22 @@
 import { eq } from 'drizzle-orm';
+import Sqids from 'sqids';
 
 import { db, User, UserData } from '~/database';
+
+const alphabet = 'koPcy5IwDTumjbOUZ8Vq1dCXnNGfxzsvrFYKpli3RtE9Hg7S0ea6JM2WBAh4QL';
+
+const sqids = new Sqids({ alphabet, minLength: 8 });
+
+const transformUser = (user: User | undefined | null) => {
+  if (!user) {
+    return null;
+  }
+
+  return {
+    ...user,
+    id: sqids.encode([user.id]),
+  };
+};
 
 const add = async (data: UserData) => {
   const [result] = await db.insert(User).values(data);
@@ -8,13 +24,15 @@ const add = async (data: UserData) => {
     where: eq(User.id, result.insertId),
   });
 
-  return user;
+  return transformUser(user);
 };
 
-const get = async (id: number) => {
-  return await db.query.User.findFirst({
-    where: eq(User.id, id),
+const get = async (id: string) => {
+  const user = await db.query.User.findFirst({
+    where: eq(User.id, sqids.decode(id)[0]),
   });
+
+  return transformUser(user);
 };
 
 export const Users = {
