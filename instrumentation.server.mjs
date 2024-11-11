175719e88faea06370ff16d41d6730eee80f1e9d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-11 18:06:22 +0100

    fix: sentry issues?

--- instrumentation.server.mjs
+++ instrumentation.server.mjs
@@ -1,7 +1,11 @@
-import * as Sentry from '@sentry/remix';
+import process from 'node:process';
 
-Sentry.init({
-  dsn: 'https://a6b24186382873322c3509292f2b4725@o308818.ingest.us.sentry.io/4508278842130432',
-  tracesSampleRate: 1,
-  autoInstrumentRemix: true,
+import { init as initSentry } from '@sentry/remix';
+
+initSentry({
+  enabled: process.env.NODE_ENV === 'production',
+  release: process.env.COMMIT_SHA,
+  dsn: 'https://8807cc909fa73f02688d068b02808d71@o308818.ingest.us.sentry.io/4508076558843904',
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
 });
