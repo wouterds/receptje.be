fcf96781ad6b07979288af9eeef0df33f7f50602
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-11 19:31:28 +0100

    feat: tweak footer

--- src/root.tsx
+++ src/root.tsx
@@ -32,17 +32,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <div className="flex flex-1 flex-col">{children}</div>
-        <footer className="text-slate-400 text-sm p-6 sm:p-8 flex justify-between">
-          <span>
-            Made by{' '}
-            <a
-              href="https://wouterds.be"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:underline hover:text-slate-600">
-              wouterds.be
-            </a>
-          </span>
+        <footer className="text-slate-500 text-sm p-6 sm:p-8 flex justify-between">
+          <span>&copy; {new Date().getFullYear()} receptje.be</span>
           <span className="flex items-center gap-2">
             Powered by gpt-4o-mini <SiOpenai className="text-base" />
           </span>
