d2f3cdceea088f52035efce240c6ae92569ba89d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-15 00:11:08 +0100

    refactor: remove unused loader

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,21 +1,7 @@
-import { ActionFunctionArgs, json, LoaderFunctionArgs } from '@remix-run/node';
+import { ActionFunctionArgs, json } from '@remix-run/node';
 
 import { Users } from '~/database';
 import { Cookies } from '~/services';
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const cookie = await Cookies.userId.parse(request.headers.get('Cookie'));
-  if (!cookie) {
-    return json({ error: 'Unauthorized' }, { status: 401 });
-  }
-
-  const user = await Users.get(cookie);
-  if (!user) {
-    return json({ error: 'Unauthorized' }, { status: 401 });
-  }
-
-  return json({ user });
-};
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const cookie = await Cookies.userId.parse(request.headers.get('Cookie'));
