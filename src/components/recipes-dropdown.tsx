daa6ddccf0f9d85712d2b2bb4f84d1fb1c49d08b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-11-17 15:41:32 +0100

    feat: fade out bottom

--- src/components/recipes-dropdown.tsx
+++ src/components/recipes-dropdown.tsx
@@ -1,24 +1,15 @@
 import { Link } from '@remix-run/react';
-import { useRef } from 'react';
 
 import { useMe } from '~/hooks';
 
 export const RecipesDropdown = () => {
   const { recipes } = useMe();
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseEnter = () => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop = 0;
-    }
-  };
-
   if (!recipes.length) {
     return null;
   }
 
   return (
-    <div className="relative group" onMouseEnter={handleMouseEnter}>
+    <div className="relative group">
       <div className="relative transition-opacity hover:opacity-80 p-4 flex items-center gap-1.5 cursor-pointer">
         Mijn receptjes
         <span className="absolute right-0 top-1 min-w-4 h-4 p-1 bg-rose-500 rounded-full text-rose-50 flex items-center justify-center text-[10px]">
@@ -26,13 +17,13 @@ export const RecipesDropdown = () => {
         </span>
       </div>
       <div className="absolute right-0 pt-1 w-80 origin-top-right invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all">
-        <div className="rounded-lg bg-white/50 shadow-lg shadow-black/5 ring-1 ring-black/5">
-          <div ref={scrollContainerRef} className="p-2 max-h-[80vh] overflow-y-auto">
+        <div className="rounded-lg bg-[#FBFAFA] shadow-lg shadow-black/5 ring-1 ring-black/5 relative">
+          <div className="p-2 max-h-[80vh] overflow-y-auto">
             {recipes.map((recipe) => (
               <Link
                 key={recipe.id}
                 to={`/recepten/${recipe.identifier}-${recipe.id}`}
-                className="flex flex-col gap-0.5 px-4 py-3 rounded-md hover:bg-tint group/item transition-colors">
+                className="flex flex-col gap-0.5 p-3 rounded-md hover:bg-tint group/item transition-colors">
                 <span className="font-medium text-sm group-hover/item:text-rose-500 transition-colors">
                   {recipe.name}
                 </span>
@@ -40,6 +31,9 @@ export const RecipesDropdown = () => {
               </Link>
             ))}
           </div>
+          {recipes?.length > 3 && (
+            <div className="absolute bottom-0 left-0 right-0 h-12 bg-gradient-to-t from-white/50 to-transparent pointer-events-none rounded-b-lg" />
+          )}
         </div>
       </div>{' '}
     </div>
